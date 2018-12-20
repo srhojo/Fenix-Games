517348359540e72e9a34a5152ee3ba7b0074f5a0
srhojo <j.lacalle@icloud.com>
2018-12-20 23:18:25 +0100

    modfy main js

--- src/main/java/com/hojo/fenix/games/controllers/GamesAdminController.java
+++ src/main/java/com/hojo/fenix/games/controllers/GamesAdminController.java
@@ -13,6 +13,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/private/v1")
+@CrossOrigin(origins = "http://localhost:8000")
 public class GamesAdminController {
 
     @Autowired
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,60 +1,52 @@
-//Estos componentes se tendrás que recuperar del servicio http
-var components = [
-    { id: 1, value: 'Games', uri: '' },
-    { id: 2, value: 'Players', uri: '' }
+var components = [{
+        id: 1,
+        value: 'Games',
+        uri: ''
+    },
+    {
+        id: 2,
+        value: 'Players',
+        uri: ''
+    }
 ];
-
-
 
 var vmNavbar = new Vue({
     el: '#mainNavbars',
     data: {
         components: components
     }
-})
+});
 
-var vmGamesForm = new Vue({
-    el:'#gamesForm',
-    data() {
-        return {
-            game:{
-                id: null,
-                name: "",
-                description: ""
-            }
-        }
-    },
-    methods:{
-        saveGame : function () {
-            console.log('Vamos a añadir el juevo --> Nombre: '+this.game.name+', Descripcion: '+this.game.description)
+var vmGamesPage = new Vue({
+    el: '#gamesPage',
+    data:{
+        game: {
+            id: null,
+            name: "",
+            description: ""
         },
-        updateGame : function () {
-            console.log('Vamos a actualizar el juevo --> Nombre: '+this.game.name+', Descripcion: '+this.game.description)
-        }
-    }
-})
-
-var vmGamesTable = new Vue({
-    el: '#gamesTable',
-    data() {
-        return {
-            data: null,
-            loading: true,
-            arrored: false
-
+        gamesList: [],
+        gamesListErrored: false,
+        gamesListLoading: true
+    },
+    methods: {
+        saveGame: function () {
+            console.log('Vamos a añadir el juevo --> Nombre: ' + this.game.name + ', Descripcion: ' + this.game.description)
+        },
+        updateGame: function () {
+            console.log('Vamos a actualizar el juevo --> Nombre: ' + this.game.name + ', Descripcion: ' + this.game.description)
         }
     },
     mounted() {
         axios
             .get('http://localhost:8080/private/v1/games')
             .then(response => {
-                this.data = response.data;
+                this.gamesList = response.data;
             })
             .catch(error => {
                 console.log(error);
-                this.errored = true;
+                this.gamesListErrored = true;
             })
-            .finally(() => this.loading = false);
-    },
+            .finally(() => this.gamesListLoading = false);
+    }
 });
-
